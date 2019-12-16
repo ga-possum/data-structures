74383387f99749e2f19a76ce53060a0b1bc0b52f
ga-possum <aleks.khristof@gmail.com>
2019-12-15 23:48:06 -0500

    done

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,5 +1,59 @@
 var BinarySearchTree = function(value) {
+  var newTree = Object.create(binaryTreeMethods);
+  newTree.value = value;
+  newTree.left = null;
+  newTree.right = null;
+  return newTree;
 };
+
+var binaryTreeMethods = {};
+
+binaryTreeMethods.insert = function(value) {
+  //will break down if values equal to parent.value
+  if (value < this.value) {
+    //left
+    if (this.left) {
+      binaryTreeMethods.insert.call(this.left,value)
+    } else {
+    this.left = BinarySearchTree(value);
+    }
+  } else {
+    //right
+    if (this.right) {
+      binaryTreeMethods.insert.call(this.right,value)
+    } else {
+    this.right = BinarySearchTree(value);
+    }
+  }
+};
+
+binaryTreeMethods.contains = function(target) {
+  var child;
+  var found = false;
+  if (this.value === target) {
+    found = true;
+  } else if (this.left || this.right){
+    for (var i = 0; i < 2; i++) {
+      if (i === 0) {child = this.left} else {child = this.right}
+      if (!found){
+        found = binaryTreeMethods.contains.call(child, target);
+      }
+    }
+  }
+  return found;
+};
+
+binaryTreeMethods.depthFirstLog = function(callback) {
+  callback(this.value);
+  if (this.left || this.right){
+    for (var i = 0; i < 2; i++) {
+      if (i === 0) {child = this.left} else {child = this.right}
+      if (child) {
+        binaryTreeMethods.depthFirstLog.call(child, callback);
+      }
+    }
+  }
+}
 
 
 /*
--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,26 +1,53 @@
+// var Set = function() {
+//   var set = Object.create(setPrototype);
+//   set._storage = {}; // fix me
+//   return set;
+// };
+
+// var setPrototype = {};
+
+// setPrototype.add = function(item) {
+//   this._storage[item] = true;
+// };
+
+// setPrototype.contains = function(item) {
+//   for (var i in this._storage) {
+//     if (i === item) {return true}
+//   }
+//   return false;
+// };
+
+// setPrototype.remove = function(item) {
+//   for (var i in this._storage) {
+//     if (i === item) {delete this._storage[i]}
+//   }
+// };
+
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = {}; // fix me
+  set._limit = 100;
+  set._storage = LimitedArray(set._limit);
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
-  this._storage[item] = true;
+  var index = getIndexBelowMaxForKey(item, this._limit);
+  this._storage.set(index, true);
+  // var index = getIndexBelowMaxForKey(k, this._limit);
+  // this._storage.set(index, v, k);
 };
 
 setPrototype.contains = function(item) {
-  for (var i in this._storage) {
-    if (i === item) {return true}
-  }
+  var index = getIndexBelowMaxForKey(item, this._limit);
+  if (this._storage.get(index)) {return true;}
   return false;
 };
 
 setPrototype.remove = function(item) {
-  for (var i in this._storage) {
-    if (i === item) {delete this._storage[i]}
-  }
+  var index = getIndexBelowMaxForKey(item, this._limit);
+  this._storage.set(index, false);
 };
 
 
